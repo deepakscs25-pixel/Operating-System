cbb941f8dd4afc5ab90e87e91fc4dd06afbcd552
deepakscs25-pixel <deepaks.cs25@bmsce.ac.in>
2026-03-04 13:01:37 +0530

    Add files via upload

---
+++ Lab 2/SRTF.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    int at[n], bt[n], rt[n], ct[n], tat[n], wt[n], resp[n];
+    int started[n];
+
+    for(int i=0;i<n;i++){
+        printf("\nProcess P%d\n",i+1);
+        printf("Arrival Time: ");
+        scanf("%d",&at[i]);
+
+        printf("Burst Time: ");
+        scanf("%d",&bt[i]);
+
+        rt[i] = bt[i];      // remaining time
+        started[i] = 0;     // check if process started
+    }
+
+    int complete = 0, current_time = 0;
+    int min_index, min_rt;
+    float total_wt = 0, total_tat = 0, total_rt = 0;
+
+    while(complete < n){
+
+        min_index = -1;
+        min_rt = 9999;
+
+        // find process with smallest remaining time
+        for(int i=0;i<n;i++){
+            if(at[i] <= current_time && rt[i] > 0){
+                if(rt[i] < min_rt){
+                    min_rt = rt[i];
+                    min_index = i;
+                }
+            }
+        }
+
+        if(min_index == -1){
+            current_time++;
+            continue;
+        }
+
+        // first time execution → calculate RT
+        if(started[min_index] == 0){
+            resp[min_index] = current_time - at[min_index];
+            started[min_index] = 1;
+        }
+
+        rt[min_index]--;
+
+        // process finished
+        if(rt[min_index] == 0){
+            complete++;
+
+            ct[min_index] = current_time + 1;
+            tat[min_index] = ct[min_index] - at[min_index];
+            wt[min_index] = tat[min_index] - bt[min_index];
+
+            total_wt += wt[min_index];
+            total_tat += tat[min_index];
+            total_rt += resp[min_index];
+        }
+
+        current_time++;
+    }
+
+    printf("\nP\tAT\tBT\tCT\tTAT\tWT\tRT\n");
+
+    for(int i=0;i<n;i++){
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+        i+1,at[i],bt[i],ct[i],tat[i],wt[i],resp[i]);
+    }
+
+    printf("\nAverage WT = %.2f", total_wt/n);
+    printf("\nAverage TAT = %.2f", total_tat/n);
+    printf("\nAverage RT = %.2f\n", total_rt/n);
+
+    return 0;
+}
